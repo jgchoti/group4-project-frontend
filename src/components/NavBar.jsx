f87339c948af6b55f3afb325ea911d634ddce7b2
Younes Seddouki <iplusone@Youness-iMac.local>
2024-09-01 23:15:54 +0200

    firstwebpage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,59 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React from 'react';
+import './NavBar.css'; // Ensure this file is loaded correctly
+import booklogo from '../assets/booklogo.png';
+import { NavLink } from 'react-router-dom';
 
-export default function Navigation() {
+const NavBar = () => {
+  // Base style for navigation buttons
+  const baseStyle = {
+    color: '#fffcfc',
+    backgroundColor: '#050505',
+    borderRadius: '0.375rem',
+    padding: '0.75rem 1rem',
+    textAlign: 'center',
+    transition: 'background-color 0.3s',
+    textDecoration: 'none',
+  };
+
+  // when style is active
+  const activeStyle = {
+    backgroundColor: ' #5466dd',
+    color: '#f0e5e5',
+  };
+
+  // Making active dynamic
+  const navLinkStyle = ({ isActive }) => ({
+    ...baseStyle,
+    ...(isActive ? activeStyle : {}),
+  });
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Hack Your Future</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/recipes">Recipes</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar">
+      <div className="container">
+        <div className="nav-content">
+          <div className="logo-container">
+            <NavLink className="logo-link" to="/index">
+              <img
+                className="logo-img"
+                src={booklogo}
+                alt="BookMate"
+              />
+              <span className="logo-text">BookMate</span>
+            </NavLink>
+            <div className="nav-links">
+              <div className="links">
+                <NavLink to="/" style={navLinkStyle}>Home</NavLink>
+                <NavLink to="/books" style={navLinkStyle}>Books</NavLink>
+                <NavLink to="/add-book" style={navLinkStyle}>Add Book</NavLink>
+                <NavLink to="/about" style={navLinkStyle}>About us</NavLink>
+                <NavLink to="/login" style={navLinkStyle}>Login</NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
+
+export default NavBar;
