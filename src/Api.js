7ce7e7eee16206e336d65541afe2703d967e380b
jgchoti <jgchotirat@gmail.com>
2024-09-04 01:00:14 +0200

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -9,3 +9,21 @@
 // };
 
 // export default Api;
+
+const Api = async (endpoint) => {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    console.log("url", apiUrl)
+    try {
+        const response = await fetch(`${apiUrl}/${endpoint}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+};
+
+export default Api;
