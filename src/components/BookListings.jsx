ed6b60d909899dcd23d05322c844e9df4b5d4f11
jgchoti <jgchotirat@gmail.com>
2024-09-04 00:59:58 +0200

    Update BookListings.jsx

--- src/components/BookListings.jsx
+++ src/components/BookListings.jsx
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./BookListings.css";
 import george from "../assets/george.jpg";
-import orientalism from '../assets/orientalism.jpg';
-import secret from '../assets/secret.jpg';
+import orientalism from "../assets/orientalism.jpg";
+import secret from "../assets/secret.jpg";
 import BookListing from "./BookListing";
-import books from '../books.json'
+import Api from "../Api";
 
 const BookListings = () => {
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    Api("books")
+      .then((data) => {
+        setBooks(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   const images = {
-    'george.jpg': george,
-    'orientalism.jpg': orientalism,
-    'secret.jpg': secret
+    "george.jpg": george,
+    "orientalism.jpg": orientalism,
+    "secret.jpg": secret,
   };
 
-  const recentBooks = books.slice(0,3);
+  if (!books) {
+    return <div>Loading...</div>;
+  }
 
+  const recentBooks = books.slice(0, 3);
   return (
     <section className="book-listings">
-        <div className="recent">
-          <h1> Recently Added Books </h1>
-        </div>
+      <div className="recent">
+        <h1> Recently Added Books </h1>
+      </div>
       <div className="book-listings-container">
         {recentBooks.map((book) => (
-          <BookListing
-            key={book.id}
-            book={book}
-            image={images[book.image]}  
-          />
+          <BookListing key={book.id} book={book} image={images[book.image]} />
         ))}
       </div>
     </section>
@@ -34,4 +43,3 @@ const BookListings = () => {
 };
 
 export default BookListings;
-
