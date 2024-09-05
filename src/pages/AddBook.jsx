1af0c1c4aa0f1c1fe3c8f97d79fcc9d71cc29c47
jgchoti <jgchotirat@gmail.com>
2024-09-05 22:49:47 +0200

    Update AddBook.jsx

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import UploadImage from "../components/UploadImage";
+import { NavLink } from "react-router-dom";
 
 const AddBook = () => {
-  return (
-    <div>AddBook</div>
-  )
-}
+  const [token, setToken] = useState(null);
 
-export default AddBook
+  useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+    setToken(savedToken);
+  }, []);
+
+  if (!token) {
+    return (
+      <div>
+        <p>Please log in to upload images.</p>
+        <NavLink to="/login">Login</NavLink>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Add a Book</h1>
+      <UploadImage token={token} />
+    </div>
+  );
+};
+
+export default AddBook;
