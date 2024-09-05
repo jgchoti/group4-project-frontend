47c052603a7fc0017f99c9b20160a4d7278ece5f
jgchoti <jgchotirat@gmail.com>
2024-09-05 22:49:50 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import LoginForm from "../components/LoginForm";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
