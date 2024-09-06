7578265fd8abab564135213480a4c73bcb106b58
jgchoti <jgchotirat@gmail.com>
2024-09-07 00:45:17 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LoginForm from "../components/LoginForm";
 
 const Login = () => {
+  const [loginSuccess, setLoginSuccess] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setLoginSuccess(Boolean(token));
+  }, []);
+
+  if (loginSuccess) {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Login</h1>
