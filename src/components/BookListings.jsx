d8ce29c814145af7edc2973d696895597b43c77a
jgchoti <jgchotirat@gmail.com>
2024-09-07 00:44:24 +0200

    Update BookListings.jsx

--- src/components/BookListings.jsx
+++ src/components/BookListings.jsx
@@ -10,32 +10,33 @@ const BookListings = () => {
   const [books, setBooks] = useState(null);
 
   useEffect(() => {
-    Api("books/all")
+    Api("books/recent")
       .then((data) => {
         setBooks(data);
       })
       .catch((error) => console.error("Error:", error));
   }, []);
+  console.log(books);
 
-  const images = {
-    "george.jpg": george,
-    "orientalism.jpg": orientalism,
-    "secret.jpg": secret,
-  };
+  // const images = {
+  //   "george.jpg": george,
+  //   "orientalism.jpg": orientalism,
+  //   "secret.jpg": secret,
+  // };
 
   if (!books) {
     return <div>Loading...</div>;
   }
 
-  const recentBooks = books.slice(0, 3);
+  // const recentBooks = books.slice(0, 3);
   return (
     <section className="book-listings">
       <div className="recent">
         <h1> Recently Added Books </h1>
       </div>
       <div className="book-listings-container">
-        {recentBooks.map((book) => (
-          <BookListing key={book.id} book={book} image={images[book.image]} />
+        {books.map((book) => (
+          <BookListing key={book.id} book={book} image={book.image} />
         ))}
       </div>
     </section>
