6ea2dc89c9496453c363e3fe3df53946bfc29511
jgchoti <jgchotirat@gmail.com>
2024-09-09 00:13:37 +0200

    Update BookListing.jsx

--- src/components/BookListing.jsx
+++ src/components/BookListing.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import {useState} from 'react';
+import { useState } from "react";
 
 const BookListing = ({ book, image }) => {
-
   const [showMore, setShowMore] = useState(false);
 
-  const toggleShowMore = () =>{
+  const toggleShowMore = () => {
     setShowMore(!showMore);
-  }
+  };
   return (
     <article className="book-item">
       <div className="image-wrapper">
@@ -19,26 +18,26 @@ const BookListing = ({ book, image }) => {
         <h3>Author:</h3>
         <p>{book.author}</p>
         {showMore && (
-             <>
-             <h3>Genre:</h3>
-             <p>{book.genre}</p>
-             <h3>Condition:</h3>
-             <p>{book.condition}</p>
-             <h3>Price:</h3>
-             <p>{book.price}</p>
-             <h3>Email Address:</h3>
-             <p>{book.email}</p>
-             <h3>Phone Number:</h3>
-             <p>{book.phone}</p>
-             <h3>Location:</h3>
-             <p>{book.city}</p>
-             <h3>Delivery Method:</h3>
-             <p>{book.delivery}</p>
-             <h3>Additional Info:</h3>
-             <p>{book.information}</p>
-             </>
+          <>
+            <h3>Genre:</h3>
+            <p>{book.genre}</p>
+            <h3>Condition:</h3>
+            <p>{book.book_condition}</p>
+            <h3>Price:</h3>
+            <p>€{book.price}</p>
+            <h3>Email Address:</h3>
+            <p>{book.seller_email}</p>
+            <h3>Phone Number:</h3>
+            <p>{book.seller_phonenumber}</p>
+            <h3>Location:</h3>
+            <p>{book.city}</p>
+            <h3>Delivery Method:</h3>
+            <p>{book.delivery}</p>
+            <h3>Additional Info:</h3>
+            <p>{book.information}</p>
+          </>
         )}
-      <button onClick={toggleShowMore} className="toggle-button">
+        <button onClick={toggleShowMore} className="toggle-button">
           {showMore ? "Show Less" : "Show More"}
         </button>
       </div>
