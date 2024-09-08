c07dab0c711cebcecbb40358d905ab07823713a2
jgchoti <jgchotirat@gmail.com>
2024-09-08 22:08:44 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,9 +26,6 @@ const Login = () => {
     <div>
       <h1>Login</h1>
       <LoginForm />
-      <p>
-        Don't have an account? <a href="/register">Register</a>
-      </p>
     </div>
   );
 };
