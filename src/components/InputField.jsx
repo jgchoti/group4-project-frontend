abdec13d5040597928b28087c69ed0783e021e50
jgchoti <jgchotirat@gmail.com>
2024-09-09 09:53:50 +0200

    Update InputField.jsx

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
-const InputField = ({ type, name, value, onChange, placeholder }) => {
+const InputField = ({
+  type,
+  name,
+  value,
+  onChange,
+  placeholder,
+  label,
+  required = false,
+}) => {
   return (
     <div className="input-field">
+      {label && <label htmlFor={name}>{label}</label>}
       <input
         type={type}
         name={name}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        required
+        required={required}
       />
     </div>
   );
