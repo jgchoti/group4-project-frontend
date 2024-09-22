ea791bbba3338ff4a8411f53bbb579fe2bea8dcb
Tesfay20 <tesfay.haftu1989@gmail.com>
2024-09-22 13:27:08 +0200

    added BooskPage

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import Api from '../Api';
 import { Link } from 'react-router-dom';
+import BookListing from '../components/BookListing';
 
 const BooksPage = () => {
   const [books, setBooks] = useState([]);
@@ -8,9 +9,10 @@ const BooksPage = () => {
   
   useEffect(() => {
     
-    axios.get('http://localhost:5001/api/books')
+    Api("books/all")
       .then(response => {
-        setBooks(response.data);
+        console.log(response)
+        setBooks(response);
       })
       .catch(error => {
         console.error('Error fetching books:', error);
@@ -20,20 +22,14 @@ const BooksPage = () => {
   return (
     <div>
       <h1>Books</h1>
-      <div className="books-list">
+      <div className="book-listings-container">
+
         {books.map((book) => (
-          <div key={book.book.id} className="book-item">
-            <img src={book.image_url} alt={book.title} style={{ width: '150px', height: '200px' }} />
-            <h3>{book.title}</h3>
-            <p>Author: {book.author}</p>
-            <p>Condition: {book.condition}</p>
-            <p>Genre: {book.genre}</p>
-            <p>Published Year: {book.publishedYear}</p>
-            <p>Price: €{book.price}</p>
-            
-            <Link to={`/books/${book.id}`}>View Details</Link>
-          </div>
+        <BookListing key = {book.id}
+        book = {book} 
+        image = {book.image} />
         ))}
+
       </div>
     </div>
   );
