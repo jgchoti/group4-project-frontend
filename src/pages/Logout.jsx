0d1e4bb16f218d7891ee8144be68dd9568b9d6fb
jgchoti <jgchotirat@gmail.com>
2024-09-22 23:10:32 +0200

    Update Logout.jsx

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { NavLink,Route,Router, Routes} from "react-router-dom";
+import { NavLink, Route, Router, Routes } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Api from "../Api";
 import Login from "./Login";
 import "../components/Logout.css";
 import HomePage from "./HomePage";
 
-
 const Logout = ({ token }) => {
   const [logoutSuccess, setLogoutSuccess] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -23,6 +24,9 @@ const Logout = ({ token }) => {
       localStorage.removeItem("token");
       localStorage.removeItem("user");
       setLogoutSuccess(true);
+      setTimeout(() => {
+        navigate("/"); // Redirect to home after logout
+      }, 2000);
     } catch (error) {
       console.error("Error logging out:", error);
     }
@@ -31,22 +35,18 @@ const Logout = ({ token }) => {
   return (
     <div>
       {logoutSuccess ? (
-        <div>        <div>
-
-            <h3>Logout Successfully! </h3>        </div>
-
-          <Routes>
-          <Route exact path="/" element={<HomePage />} />
-        </Routes>
+        <div>
+          <h3>Logout Successfully! </h3>{" "}
+          <p>You will be redirected shortly...</p>
         </div>
       ) : (
         <div className="btn1">
+          <p1>Are you sure your want to logout ?</p1>
 
-        <p1>Are you sure your want to logout ?</p1>
-        
-        <div className="btn">
-
-        <button1 onClick={handleLogout}>Logout</button1></div></div>
+          <div className="btn">
+            <button1 onClick={handleLogout}>Logout</button1>
+          </div>
+        </div>
       )}
     </div>
   );
