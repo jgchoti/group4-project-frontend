a819ff2723ab0947897cb4fc196be12076f2c332
Tesfay20 <tesfay.haftu1989@gmail.com>
2024-09-22 11:14:09 +0200

    added BooksPage

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const BooksPage = () => {
-  return (
-    <div>BooksPage</div>
-  )
-}
+  const [books, setBooks] = useState([]);
+  
+  
+  useEffect(() => {
+    
+    axios.get('http://localhost:5001/api/books')
+      .then(response => {
+        setBooks(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching books:', error);
+      });
+  }, []);
 
-export default BooksPage
+  return (
+    <div>
+      <h1>Books</h1>
+      <div className="books-list">
+        {books.map((book) => (
+          <div key={book.book.id} className="book-item">
+            <img src={book.image_url} alt={book.title} style={{ width: '150px', height: '200px' }} />
+            <h3>{book.title}</h3>
+            <p>Author: {book.author}</p>
+            <p>Condition: {book.condition}</p>
+            <p>Genre: {book.genre}</p>
+            <p>Published Year: {book.publishedYear}</p>
+            <p>Price: €{book.price}</p>
+            
+            <Link to={`/books/${book.id}`}>View Details</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BooksPage;
+
+
