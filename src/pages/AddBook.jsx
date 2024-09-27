afd3c881ec389b790253cc8b054b447b49ab4b50
jgchoti <jgchotirat@gmail.com>
2024-09-27 15:17:43 +0200

    Update AddBook.jsx

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { NavLink,Route,Router, Routes} from "react-router-dom";
-import AddBookForm from "../components/AddBookForm";
+import { NavLink, Route, Router, Routes } from "react-router-dom";
+import AddBookForm from "../components/AddbookForm";
 import { Forward } from "react-bootstrap-icons";
 import Login from "./Login";
 
@@ -19,7 +19,7 @@ const AddBook = () => {
         <Routes>
           <Route exact path="/" element={<Login />} />
         </Routes>
-    </div>
+      </div>
     );
   }
 
